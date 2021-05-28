f0802e96dd5d850039e6924ec40c7494ff7f6dd3
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-05-27 22:39:09 -0500

    Add files via upload

---
+++ WeSplit2/WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by paul taylor on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

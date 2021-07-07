0d3366221228c23b3a4eae2decf4a04753af61c0
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-07-06 22:13:22 -0500

    Add files via upload

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @State private var scoreTitle = "" // title inside the alert
     @State private var score = 0
     
+    @State private var animationAmount = 0.0
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]),
@@ -29,31 +31,39 @@ struct ContentView: View {
                         .fontWeight(.black)
                     
                 }
-                
+  
                 ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                    FlagButton(country: self.countries[number]) {
+                        flagTapped(number)
                     }
+                    .rotation3DEffect(.degrees(number == correctAnswer ? animationAmount: 0), axis: (x: 0, y: 1, z: 0))
                 }
                 Spacer()
             }
         }
+                
+        
+                 
+                        
+                    
+         
         .alert(isPresented: $showingScore) {
             Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
     }
+    
+
+    
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-                scoreTitle = "Correct"
-                score += 100
+            scoreTitle = "Correct"
+            score += 100
+                withAnimation {
+                    self.animationAmount += 360
+               }
+    
         } else {
             scoreTitle = "Wrong. This was the flag of \(countries[number])"
             score -= 100
@@ -61,9 +71,26 @@ struct ContentView: View {
         showingScore = true
         
     }
+    
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+struct FlagButton: View {
+    var country: String
+    var onTap: () -> ()
+    var body: some View {
+        Button(action: {
+            self.onTap()
+        }) {
+    Image(country)
+        .renderingMode(.original)
+        .clipShape(Capsule())
+        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+        .shadow(color: .black, radius: 2)
+        }
     }
 }
 

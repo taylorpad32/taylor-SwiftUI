4c5ddd015cf5d80301fdb4e97da414fc422ba4d0
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-07-11 14:19:38 -0500

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by paul taylor on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

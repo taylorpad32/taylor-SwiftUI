9c03dfe5711f4c81d2d85f9d16a1569e3b22a597
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-07-11 14:19:05 -0500

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by paul taylor on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

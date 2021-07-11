da552cd6fb4cc3530e55f9c18117c20f3f9f030c
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-07-11 14:20:46 -0500

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by paul taylor on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
